c34ea04dc990cdca9c9ae0a2e808b10f01b5b766
Ronald Pereira <ronald.drp11@gmail.com>
2017-11-03 11:38:32 -0200

    Saving code progress

--- src/code/client.py
+++ src/code/client.py
@@ -1,21 +1,46 @@
 import sys
 import socket
 import select
-import queue
-import time
+
+SERVERID = (65535).to_bytes(2, byteorder='big') # ID do servidor = 65535
+BUFSIZE = 65535 # Maximum buffer size
+
+class MessageTypes:
+	def getMessageType(type):
+		messageTypes = {
+						'OK': (1).to_bytes(2, byteorder='big'),
+						'ERRO': (2).to_bytes(2, byteorder='big'),
+						'OI': (3).to_bytes(2, byteorder='big'),
+						'FLW': (4).to_bytes(2, byteorder='big'),
+						'MSG': (5).to_bytes(2, byteorder='big'),
+						'CREQ': (6).to_bytes(2, byteorder='big'),
+						'CLIST': (7).to_bytes(2, byteorder='big')
+						}
+
+		return messageTypes[type]
+
+class Communication:
+	def __init__(self):
+		self.clientID = 0
+		self.seqnum = 0
+
+	def headerConstructor(self, type, origin, destiny, seqnum):
+		return MessageTypes.getMessageType(type) + origin.to_bytes(2, byteorder='big') + destiny.to_bytes(2, byteorder='big') + seqnum.to_bytes(2, byteorder='big')
+
+	def sendOI(client):
+		message = Communication.headerConstructor('OI', self.clientID, SERVERID, self.seqnum)
+		client.send(message)
+		recvMessage = client.recv(BUFSIZE)
+		self.clientID = recvMessage[16:32]
+		self.seqnum += 1
+
 
 host = sys.argv[1]
 port = int(sys.argv[2])
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.connect((host, port))
-print("Successfully connected")
+client.connect((host, port))
+print("Successfully connected on %s:%d" %(host, port))
 
-message = "Hello. I'm connected".encode()
-
-sock.send(message)
-
-time.sleep(10)
-
-sock.send(message)
+chat = Communication()
--- src/code/server.py
+++ src/code/server.py
@@ -1,26 +1,48 @@
 import sys
 import socket
 import select
-import queue
+
+SERVERID = (65535).to_bytes(2, byteorder='big') # ID do servidor = 65535
+BUFSIZE = 65535 # Maximum buffer size
+
+class MessageTypes:
+	def getMessageType(type):
+		messageTypes = {
+						'OK': (1).to_bytes(2, byteorder='big'),
+						'ERRO': (2).to_bytes(2, byteorder='big'),
+						'OI': (3).to_bytes(2, byteorder='big'),
+						'FLW': (4).to_bytes(2, byteorder='big'),
+						'MSG': (5).to_bytes(2, byteorder='big'),
+						'CREQ': (6).to_bytes(2, byteorder='big'),
+						'CLIST': (7).to_bytes(2, byteorder='big')
+						}
+
+		return messageTypes[type]
 
 host = '127.0.0.1'
 port = int(sys.argv[1])
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sock.bind((host, port))
+server.bind((host, port))
 print("Server started and listening.")
 
-sock.listen(65534)
+server.listen(255)
+
+hostList = [server, sys.stdin]
 
 while 1:
-	(clientSocket, adress) = sock.accept()
-	print("Connected on a client.")
+	(clientSocket, address) = server.accept()
+	print("Connected on client %d from %s." %(clientSocket.fileno(), address))
+	hostList.append(clientSocket)
 
-	message = clientSocket.recv(65535).decode()
+	print(hostList)
 
-	print(message)
-
-	message = clientSocket.recv(65535).decode()
-
-	print("Last message:"+message)
+	inputSockets, outputSockets, error = select.select(hostList, hostList, [])
+	print(inputSockets)
+	# print(message)
+	#
+	# message = clientSocket.recv(65535).decode()
+	#
+	# print("Last message:"+message)
