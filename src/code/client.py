2eb4edded326bf7c58d3e4d19758392f9b1aa19c
Ronald Pereira <ronald.drp11@gmail.com>
2017-11-04 16:21:52 -0200

    Project finished and commented

--- src/code/client.py
+++ src/code/client.py
@@ -1,13 +1,18 @@
 import sys
 import socket
 import select
-import binascii
 
 SERVERID = 65535 # ID do servidor = 65535
 BUFSIZE = 400 # Maximum buffer size
 
-class MessageTypes:
-	def getMessageType(type):
+# OBSERVATION ABOUT SOCKET.FILENO():
+# The socket.fileno() function returns a integer that means the file descriptor, starting from 4.
+# That happens because UNIX reserves the first three descriptors to sys.stdin, sys.stdout and the actual socket.
+# So, for clients to have the IDs starting from 1, this code always does the conversion of socket.fileno() + 3 for receiving IDs from clients and socket.fileno() - 3 for sending IDs for clients.
+# Client side : IDs starts from 1 / Server side : IDs starts from 4
+
+class MessageTypes: # Dictionary to store the message types
+	def getMessageType(type): # Method to return the 2 bytes for the message type given
 		messageTypes = {
 						'OK': (1).to_bytes(2, byteorder='big'),
 						'ERRO': (2).to_bytes(2, byteorder='big'),
@@ -15,25 +20,29 @@ class MessageTypes:
 						'FLW': (4).to_bytes(2, byteorder='big'),
 						'MSG': (5).to_bytes(2, byteorder='big'),
 						'CREQ': (6).to_bytes(2, byteorder='big'),
-						'CLIST': (7).to_bytes(2, byteorder='big')
+						'CLIST': (7).to_bytes(2, byteorder='big'),
+						'OIAP': (13).to_bytes(2, byteorder='big'),
+						'MSGAP': (15).to_bytes(2, byteorder='big'),
+						'CREQAP': (16).to_bytes(2, byteorder='big'),
+						'CLISTAP': (17).to_bytes(2, byteorder='big')
 						}
 
 		return messageTypes[type]
 
 
-class Communication:
+class Communication: # Contains all the communication methods
 	def __init__(self):
-		self.clientID = 0
-		self.seqnum = 0
+		self.clientID = 0 # Saves the client side given ID for each client
+		self.seqnum = 0 # Saves the sequence number of the message
 
-	def headerConstructor(self, type, destiny):
+	def headerConstructor(self, type, destiny): # Contruct the header of all message types, except MSGAP
 		return MessageTypes.getMessageType(type) + self.clientID.to_bytes(2, byteorder='big') + destiny.to_bytes(2, byteorder='big') + self.seqnum.to_bytes(2, byteorder='big')
 
-	def sendOK(self, client):
+	def sendOK(self, client): # Method to send OK message to the server
 		message = self.headerConstructor('OK', SERVERID)
 		client.send(message)
 
-	def sendOI(self, client):
+	def sendOI(self, client): # Method to send OI message to the server
 		message = self.headerConstructor('OI', SERVERID)
 		client.send(message)
 		recvMessage = client.recv(BUFSIZE)
@@ -41,38 +50,48 @@ class Communication:
 		if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
 			self.clientID = int.from_bytes(recvMessage[4:6], byteorder='big')
 			self.seqnum += 1
-			print("Connected successfully! Your ID is:", self.clientID)
+			print("Your ID is:", self.clientID)
 
-	def sendFLW(self, client):
+		elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+			print('ERROR!')
+
+	def sendFLW(self, client): # Method to send FLW message to the server
 		message = self.headerConstructor('FLW', SERVERID)
 		client.send(message)
 		recvMessage = client.recv(BUFSIZE)
 
 		if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
+			self.seqnum += 1
 			client.close()
 			print("Connection closed successfully!")
 
-	def sendCREQ(self, client):
+		elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+			print('ERROR!')
+
+	def sendCREQ(self, client): # Method to send Client REQuirement message to the server
 		message = self.headerConstructor('CREQ', SERVERID)
 		client.send(message)
 		recvMessage = client.recv(BUFSIZE)
 
 		if recvMessage[0:2] == MessageTypes.getMessageType('CLIST'):
+			self.seqnum += 1
 			numberClients = int.from_bytes(recvMessage[8:10], byteorder='big')
 			print("Number of connected clients:",numberClients)
 
-			for i in range(1,numberClients+1):
+			print('Connected IDs:',end=' ')
+
+			for i in range(1,numberClients+1): # For each client ID received, print it
 				print(int.from_bytes(recvMessage[(8+i*2):(10+i*2)], byteorder='big'), end=' ')
 			print('')
 
 		message = self.headerConstructor('OK', SERVERID)
 		client.send(message)
 
-	def sendMSG(self, client, destiny, text):
+	def sendMSG(self, client, destiny, text): # Method to send a MSG message to the destiny socket
 		message = self.headerConstructor('MSG', destiny)
 		message += len(text).to_bytes(2, byteorder='big')
 
-		for c in text:
+		for c in text: # For each character in the text, convert it to a byte and concatenate it to the message
 			message += int(c).to_bytes(1, byteorder='big')
 
 		client.send(message)
@@ -80,34 +99,89 @@ class Communication:
 		recvMessage = client.recv(BUFSIZE)
 
 		if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
+			self.seqnum += 1
+			print("Message delivered.")
+
+		elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+			print("ERROR!")
+
+	def sendOIAP(self, client, nickname=''): # Method to send a OIAP message to the server. Note here that the nickname is an empty string by default, unless it is sent by the function caller
+		message = self.headerConstructor('OIAP', SERVERID)
+
+		if nickname: # If the nickname exists
+			message += nickname # Concatenate the nickname to the message to be sent for the server. Using this, you will set your actual nickname in the server side
+
+		client.send(message)
+
+		recvMessage = client.recv(BUFSIZE)
+
+		if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
+			self.seqnum += 1
+			print("Your nickname is:", recvMessage[8:].decode()) # Prints the nickname received from the server side
+
+		elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+			print("ERROR!")
+
+	def sendCREQAP(self, client): # Method to send a Client REQuirement AP (nickname) message to the server
+		message = self.headerConstructor('CREQAP', SERVERID)
+		client.send(message)
+
+		recvMessage = client.recv(BUFSIZE)
+
+		if recvMessage[0:2] == MessageTypes.getMessageType('CLISTAP'):
+			self.seqnum += 1
+			numberNicknameClients = int.from_bytes(recvMessage[8:10], byteorder='big')
+			print("Number of connected clients with nicknames:",numberNicknameClients)
+
+			for c in recvMessage[10:]: # For each character in the text, separated by '-', print the socket ID and the socket nickname
+				if chr(c) == '-':
+					print('')
+				else:
+					print(chr(c), end='')
+			print('')
+
+		message = self.headerConstructor('OK', SERVERID)
+		client.send(message)
+
+	def sendMSGAP(self, client, destinyNickname, text): # Method to send a MSGAP message to the server
+		message = MessageTypes.getMessageType('MSGAP') + self.clientID.to_bytes(2, byteorder='big') + SERVERID.to_bytes(2, byteorder='big') + self.seqnum.to_bytes(2, byteorder='big') + len(text).to_bytes(2, byteorder='big') # Normal header of a MSG type, but using a MSGAP type instead
+		message += destinyNickname.encode('ascii') + '-'.encode('ascii') + text # Concatenate the destiny nickname with the text message, separated by a '-'
+
+		client.send(message)
+
+		recvMessage = client.recv(BUFSIZE)
+
+		if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
+			self.seqnum += 1
 			print("Message delivered.")
 
 		elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
 			print("ERROR!")
 
 
-host = sys.argv[1]
-port = int(sys.argv[2])
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = sys.argv[1] # Server address received by command line argument
+port = int(sys.argv[2]) # Server port received by command line argument
 
-client.connect((host, port))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Alocate a socket using TCP protocol and IPv4
+
+client.connect((host, port)) # Connect the client to the server
 print("Successfully connected on %s:%d" %(host, port))
 
-chat = Communication()
+chat = Communication() # Create an object of Communication to store all the data and methods of this specific client on the client side
 
-chat.sendOI(client)
+chat.sendOI(client) # Send an OI message to the server, so it outputs the client ID
 
-inputList = [client, sys.stdin]
+inputList = [client, sys.stdin] # Possible input list for the client side
 
-active = True
+active = True # Flag for the client to continue running
 
 while active:
-	inputSockets, outputSockets, error = select.select(inputList, inputList, [])
+	inputSockets, outputSockets, error = select.select(inputList, inputList, []) # Get all the ready inputSockets and outputSockets
 
-	for sock in inputSockets:
+	for sock in inputSockets: # For each socket to be read
 
-		if sock == client:
+		if sock == client: # If the socket is the client socket, tries to receive a message from the server
 			recvMessage = client.recv(BUFSIZE)
 
 			print("Message from [%d]:" %(int.from_bytes(recvMessage[2:4], byteorder='big')), end=' ')
@@ -118,23 +192,43 @@ while active:
 
 			chat.sendOK(client)
 
-		elif sock == sys.stdin:
-			data = sys.stdin.readline().split('\n')[0]
-			data = data.split(' ')
+		elif sock == sys.stdin: # If the socket is the sys.stdin, tries to receive a command
+			data = sys.stdin.readline().split('\n')[0] # Gets the command line from the sys.stdin input, without the '\n'
+			data = data.split(' ') # Split the data from the sys.stdin input to be easier to analyze
 
-			if data[0].upper() == 'OI':
+			if data[0].upper() == 'OI': # If the received command is a OI type
 				chat.sendOI(client)
+				print('')
 
-			elif data[0].upper() == 'FLW':
+			elif data[0].upper() == 'FLW': # If the received command is a FLW type
 				chat.sendFLW(client)
-				active = False
+				active = False # Sets the running flag to be False, so the client will shutdown
 
-			elif data[0].upper() == 'CREQ':
+			elif data[0].upper() == 'CREQ': # If the received command is a CREQ type
 				chat.sendCREQ(client)
+				print('')
 
-			elif data[0].upper() == 'MSG':
-				text = " ".join(data[4:]).encode('ascii')
+			elif data[0].upper() == 'MSG': # If the received command is a MSG type
+				text = " ".join(data[4:]).encode('ascii') # Joins all the text message into a text variable, separating each string by a space and encoding it to ascii
 				chat.sendMSG(client, int(data[2]), text)
+				print('')
+
+			elif data[0].upper() == 'OIAP': # If the received command is a OIAP type
+				if data[1:]:
+					nickname = " ".join(data[1:]).encode('ascii') # Joins all the nickname message into a nickname variable, separating each string by a space and encoding it to ascii
+					chat.sendOIAP(client, nickname)
+				else:
+					chat.sendOIAP(client)
+				print('')
+
+			elif data[0].upper() == 'CREQAP': # If the received command is a CREQAP type
+				chat.sendCREQAP(client)
+				print('')
+
+			elif data[0].upper() == 'MSGAP': # If the received command is a MSGAP type
+				text = " ".join(data[4:]).encode('ascii') # Joins all the text message into a text variable, separating each string by a space and encoding it to ascii
+				chat.sendMSGAP(client, data[2], text)
+				print('')
 
 
-client.close()
+client.close() # Close the client socket connection
--- src/code/server.py
+++ src/code/server.py
@@ -5,8 +5,14 @@ import select
 SERVERID = 65535 # ID do servidor = 65535
 BUFSIZE = 400 # Maximum buffer size
 
-class MessageTypes:
-	def getMessageType(type):
+# OBSERVATION ABOUT SOCKET.FILENO():
+# The socket.fileno() function returns a integer that means the file descriptor, starting from 4.
+# That happens because UNIX reserves the first three descriptors to sys.stdin, sys.stdout and the actual socket.
+# So, for clients to have the IDs starting from 1, this code always does the conversion of socket.fileno() + 3 for receiving IDs from clients and socket.fileno() - 3 for sending IDs for clients.
+# Client side : IDs starts from 1 / Server side : IDs starts from 4
+
+class MessageTypes: # Dictionary to store the message types
+	def getMessageType(type): # Method to return the 2 bytes for the message type given
 		messageTypes = {
 						'OK': (1).to_bytes(2, byteorder='big'),
 						'ERRO': (2).to_bytes(2, byteorder='big'),
@@ -14,48 +20,66 @@ class MessageTypes:
 						'FLW': (4).to_bytes(2, byteorder='big'),
 						'MSG': (5).to_bytes(2, byteorder='big'),
 						'CREQ': (6).to_bytes(2, byteorder='big'),
-						'CLIST': (7).to_bytes(2, byteorder='big')
+						'CLIST': (7).to_bytes(2, byteorder='big'),
+						'OIAP': (13).to_bytes(2, byteorder='big'),
+						'MSGAP': (15).to_bytes(2, byteorder='big'),
+						'CREQAP': (16).to_bytes(2, byteorder='big'),
+						'CLISTAP': (17).to_bytes(2, byteorder='big')
 						}
 
 		return messageTypes[type]
 
 
-class Communication:
-	def headerConstructor(type, destiny, seqnum):
+class Communication: # Contains all the communication methods
+	def headerConstructor(type, destiny, seqnum): # Contruct the header of all message types, except MSGAP
 		return MessageTypes.getMessageType(type) + SERVERID.to_bytes(2, byteorder='big') + destiny.to_bytes(2, byteorder='big') + seqnum.to_bytes(2, byteorder='big')
 
-	def sendOK(clientSocket):
-		message = Communication.headerConstructor('OK', clientSocket.fileno()-3, 0)
-		clientSocket.send(message)
+	def sendOK(origin): # Method to send OK to the origin socket
+		message = Communication.headerConstructor('OK', origin.fileno()-3, 0)
+		origin.send(message)
 
-	def sendERRO(clientSocket):
-		message = Communication.headerConstructor('ERRO', clientSocket.fileno()-3, 0)
-		clientSocket.send(message)
+	def sendERRO(origin): # Method to send ERRO to the origin socket
+		message = Communication.headerConstructor('ERRO', origin.fileno()-3, 0)
+		origin.send(message)
 
-	def sendCLIST(clientSocket, hostList):
-		message = Communication.headerConstructor('CLIST', clientSocket.fileno()-3, 0)
+	def sendCLIST(origin, hostList): # Method to send the ClientLIST (just the IDs) to the origin socket
+		message = Communication.headerConstructor('CLIST', origin.fileno()-3, 0)
 
 		message += len(hostList[2:]).to_bytes(2, byteorder='big')
 
-		for sock in hostList[2:]:
+		for sock in hostList[2:]: # Get each socket in the hostList and concatenate it to the message
 			message += (sock.fileno()-3).to_bytes(2, byteorder='big')
 
-		clientSocket.send(message)
+		origin.send(message)
 
-		recvMessage = clientSocket.recv(BUFSIZE)
+		recvMessage = origin.recv(BUFSIZE) # Wait for the OK confirmation
 
 		if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
 			return
 
-	def sendMSG(origin, message, hostList):
+		elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+			print('ERROR!')
+
+	def sendMSG(origin, message, hostList): # Method to send the message from the origin client to the destiny client
 		source = int.from_bytes(message[2:4], byteorder='big') + 3
-		destiny = int.from_bytes(message[4:6], byteorder='big')
+		destiny = int.from_bytes(message[4:6], byteorder='big') + 3
 
-		if destiny > 0:
-			destiny += 3
+		if source == destiny: # If the source of the message is the same of the destiny, it returns an error message
+			Communication.sendERRO(origin)
+			return
 
-			for host in hostList:
-				if host.fileno() == destiny:
+		if destiny > 3: # If the destiny is greater than 3 (fileno() = 0), it is a unicast
+			for host in hostList: # For each host in the hostList
+				if host.fileno() == destiny: # If the fileno() of the actual host is the same as the destiny, he is the socket to send the message to
+					# host.settimeout(5.0)
+				    # try:
+				    # 	recvMessage = host.recv(BUFSIZE)
+					#
+				    # except socket.timeout:
+				    # 	print('timeout')
+				    #   	Communication.sendERRO(origin)
+				    #   	host.close()
+				    #   	continue
 					host.send(message)
 					recvMessage = host.recv(BUFSIZE)
 
@@ -66,16 +90,16 @@ class Communication:
 						Communication.sendERRO(origin)
 					return
 
-			Communication.sendERRO(origin)
+			Communication.sendERRO(origin) # If none socket is equal to the destiny, returns and error message
 
-		else:
-			valid = True
-			for host in hostList:
+		elif destiny == 3: # If the destiny is 3, it means a broadcast message
+			valid = True # Valid variable to send the OK or ERRO message to the client
+			for host in hostList: # For each host in the hostList that isn't the origin, send the message
 				if host != origin:
 					host.send(message)
 					recvMessage = host.recv(BUFSIZE)
 
-					if recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+					if recvMessage[0:2] == MessageTypes.getMessageType('ERRO'): # If any message fails, I'm considering that the broadcast failed and it returns an ERRO message to the origin client
 						valid = False
 
 			if valid:
@@ -84,52 +108,154 @@ class Communication:
 			else:
 				Communication.sendERRO(origin)
 
+	def sendOKAP(origin, nicknames): # Send the nickname for the client
+	# Nicknames structure: list with tuples -> [(sock1.fileno(), sock1Nickname), sock2.fileno(), sock2Nickname, ...]
+		for nickname in nicknames: # For each tuple in nicknames list
+			if origin.fileno() == nickname[0]: # Compare the fileno() with the origin.fileno(), if it could be found, concatenate the nickname with the message
+				message = Communication.headerConstructor('OK', origin.fileno()-3, 0)
+				message += nickname[1].encode('ascii')
+				origin.send(message)
+				return
 
-host = '127.0.0.1'
-port = int(sys.argv[1])
+		Communication.sendERRO(origin) # If the nickname of the client couldn't be found, returns an ERRO message
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	def sendCLISTAP(origin, nicknames): # Method to send the ClientLIST who has nicknames in the server
+		message = Communication.headerConstructor('CLISTAP', origin.fileno()-3, 0)
+		message += len(nicknames).to_bytes(2, byteorder='big')
 
-server.bind((host, port))
+		for nickname in nicknames: # For each nickname in nicknames, concatenate the ID with the nickname, separating each set with a '-'
+			message += '-'.encode('ascii') + str((nickname[0]-3)).encode('ascii') + ' '.encode('ascii') + nickname[1].encode('ascii')
+
+		origin.send(message)
+
+		recvMessage = origin.recv(BUFSIZE)
+
+		if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
+			return
+
+		elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+			print('ERROR!')
+
+	def sendMSGAP(origin, message, nicknames, hostList): # Send a MSG type to the destiny client
+		source = int.from_bytes(message[2:4], byteorder='big') + 3
+
+		isNickname = True # Flag to distinguish between nickname and text in the received message
+		nicknameDestiny = '' # String to store the destiny nickname
+		text = '' # String to store the origin text
+		destiny = 0 # Integer to store the destiny ID
+
+		for c in message[10:]: # For each character in the received message
+			if chr(c) == '-': # If a '-' is detected, the nickname string has finished and the text started
+				isNickname = False
+
+			if isNickname: # If the actual character is the nickname
+				nicknameDestiny += chr(c)
+
+			elif not isNickname: # if the actual character is the text
+				text += chr(c)
+
+		text = text[1:] # Excludes the '-' signal from the beggining of the message
+
+		for nickname in nicknames: # For each nickname tuple in the nicknames list
+			if nickname[1] == nicknameDestiny: # If we find the nickname in the nickname list
+				destiny = nickname[0] # We save the destiny ID
+
+		if not destiny: # If the destiny stills being equal to 0, then it isn't in the nickname list, so returns an ERRO message to the origin
+			Communication.sendERRO(origin)
+
+		else: # If the destiny is different from 0, then it is valid
+			if source == destiny: # If the origin ID is the same as the destiny ID, returns an ERRO message to the origin
+				Communication.sendERRO(origin)
+				return
+
+			# This constructs the message with the MSG type to deliver to the destiny client
+			sendMessage = MessageTypes.getMessageType('MSG') + message[2:4] + destiny.to_bytes(2, byteorder='big') + message[6:8] + len(text).to_bytes(2, byteorder='big') + text.encode('ascii')
+
+			for host in hostList: # For each host in the hostList
+				if host.fileno() == destiny: # If the host is the destiny host, send the message and wait for the response
+					host.send(sendMessage)
+					recvMessage = host.recv(BUFSIZE)
+
+					if recvMessage[0:2] == MessageTypes.getMessageType('OK'):
+						Communication.sendOK(origin) # If the response from the destiny is OK, we send a OK message to the origin client
+
+					elif recvMessage[0:2] == MessageTypes.getMessageType('ERRO'):
+						Communication.sendERRO(origin) # If the response from the destiny is ERRO, we send an ERRO message to the origin client
+					return
+
+			Communication.sendERRO(origin) # If the host could not be found in the hostList, we send an ERRO message to the origin client
+
+
+host = '127.0.0.1' # Host address
+port = int(sys.argv[1]) # Port received from the command line argument
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Alocate a socket using TCP protocol and IPv4
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Option to reuse the socket address to stop giving the error: Adress is already being used when running the program multiple times
+
+server.bind((host, port)) # Bind the server to the host and the port given
 print("Server started and listening.")
 
-server.listen(255)
+server.listen(255) # Listen to a maximum of 255 paralel connections
 
-hostList = [server, sys.stdin]
+hostList = [server, sys.stdin] # Host list for the select function
+nicknames = [] # Nicknames list for the program extension
 
-active = True
+active = True # Flag for the server to continue running
 
 while active:
-	inputSockets, outputSockets, error = select.select(hostList, hostList[2:], [])
+	inputSockets, outputSockets, error = select.select(hostList, hostList[2:], []) # Get all the ready inputSockets and outputSockets (except the server and sys.stdin)
 
-	for sock in inputSockets:
+	for sock in inputSockets: # For each socket ready to be read
 
-		if sock == server:
+		if sock == server: # If the socket is the server, try to accept a new connection
 			(clientSocket, address) = server.accept()
 			print("Connected on client %d from %s." %(clientSocket.fileno()-3, address))
 
 			hostList.append(clientSocket)
 
-		elif sock == sys.stdin:
-			if sys.stdin.readline().split('\n')[0] == 'shutdown':
+		elif sock == sys.stdin: # If the socket is the sys.stdin, try to receive a command
+			if sys.stdin.readline().split('\n')[0].upper() == 'SHUTDOWN':
 				active = False
+				print('Server closed successfully.')
 
-		else:
+		else: # If the socket is not the server neither the sys.stdin, the message comes from a client socket
 			recvMessage = sock.recv(BUFSIZE)
 
-			if recvMessage[0:2] == MessageTypes.getMessageType('MSG'):
+			if recvMessage[0:2] == MessageTypes.getMessageType('MSG'): # If the received message is a MSG type
 				Communication.sendMSG(sock, recvMessage, hostList[2:])
 
-			elif recvMessage[0:2] == MessageTypes.getMessageType('OI'):
+			elif recvMessage[0:2] == MessageTypes.getMessageType('OI'): # If the received message is a OI type
 				Communication.sendOK(sock)
 
-			elif recvMessage[0:2] == MessageTypes.getMessageType('FLW'):
+			elif recvMessage[0:2] == MessageTypes.getMessageType('FLW'): # If the received message is a FLW type
 				Communication.sendOK(sock)
-				sock.close()
-				hostList.remove(sock)
+				hostList.remove(sock) # Remove the origin socket from the hostList
 
-			elif recvMessage[0:2] == MessageTypes.getMessageType('CREQ'):
+				for nickname in nicknames:
+					if nickname[0] == sock.fileno(): # If the origin socket registered a nickname, remove it from the nicknames list
+						nicknames.remove(nickname)
+
+				sock.close() # Close the connection with the origin socket
+
+			elif recvMessage[0:2] == MessageTypes.getMessageType('CREQ'): # If the received message is a CREQ type
 				Communication.sendCLIST(sock, hostList)
 
-server.close()
+			elif recvMessage[0:2] == MessageTypes.getMessageType('OIAP'): # If the received message is a OIAP type
+				if recvMessage[8:]:
+
+					for nickname in nicknames:
+						if nickname[0] == sock.fileno(): # If the origin socket already has a registered nickname, remove it from the nicknames list
+							nicknames.remove(nickname)
+
+					nicknames.append((sock.fileno(), recvMessage[8:].decode())) # And append a new one for him
+					# This avoids the same socket ID from having two different nicknames
+
+				Communication.sendOKAP(sock, nicknames)
+
+			elif recvMessage[0:2] == MessageTypes.getMessageType('CREQAP'): # If the received message is a CREAP type
+				Communication.sendCLISTAP(sock, nicknames)
+
+			elif recvMessage[0:2] == MessageTypes.getMessageType('MSGAP'): # If the received message is a MSGAP type
+				Communication.sendMSGAP(sock, recvMessage, nicknames, hostList[2:])
+
+server.close() # Close the server socket connection
